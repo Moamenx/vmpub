dca0b40c8c2e5b4ceb3d09056ec4627453997e3c
Lodenn <khalednasser788@gmail.com>
2018-05-13 21:36:34 +0200

    Design Touches

--- main/static/main/js/plugins.js
+++ main/static/main/js/plugins.js
@@ -75,5 +75,33 @@ $(function () {
         containment: "#holder",
         aspectRatio: true
     });*/
+    
+    // Our Auto Slider Code
+    (function selfSlider() {
+
+        $(".testimonials .active").each(function () {
+
+            if (!$(this).is(':last-child')) {
+
+                $(this).delay(1500).fadeOut(1000, function () {
+
+                    $(this).removeClass("active").next().addClass("active").fadeIn(1000);
+
+                    selfSlider();
+                });
+
+            } else {
+
+                $(this).delay(3000).fadeOut(1000, function () {
+
+                    $(this).removeClass("active");
+
+                    $(".testimonials ul li").eq(0).addClass("active").fadeIn(1000);
+
+                    selfSlider();
+                });
+            }
+        });
+    }());
 
 });
